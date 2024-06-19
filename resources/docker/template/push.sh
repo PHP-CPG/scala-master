7e79f2305d99b1a221bb3adfc5f186f83df5768c
Malte Wessels <malte.wessels@tu-braunschweig.de>
2024-06-19 16:50:33 +0200

    Remove deprecated file

--- resources/docker/template/push.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-id=$(docker images | grep '^scala-master' | awk '{print $3}')
-docker tag ${id} git.ias.cs.tu-bs.de/simon.koch/scala-master:latest
-docker push git.ias.cs.tu-bs.de/simon.koch/scala-master:latest
